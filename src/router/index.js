dbe6b5223de2b6935f76740ed603708b452ceede
Lucas <r.lucasmoretto@gmail.com>
2022-06-05 17:38:59 -0300

    create views and add to routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,18 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/teachers' },
-    { path: '/teachers', name: 'teachers', component: null },
+    { path: '/teachers', name: 'teachersList', component: () => import('../views/teachers/TeachersListView.vue') },
     { 
       path: '/teachers/:id', 
-      name: 'teachers', 
-      component: null,
+      name: 'teacherDetail', 
+      component: () => import('../views/teachers/TeacherDetailView.vue'),
       children: [
-        { path: '/contact', name: 'contact', component: null },
+        { path: '/contact', name: 'contact', component: () => import('../views/messages/ContactTeacherView.vue') },
       ]
     },
-    { path: '/register', name: 'register', component: null },
-    { path: '/messages', name: 'messages', component: null },
-    { path: '/:notFound(.*)', component: null }
+    { path: '/register', name: 'register', component: () => import('../views/teachers/TeacherRegistrationView.vue') },
+    { path: '/messages', name: 'messages', component: () => import('../views/messages/MessagesReceivedView.vue') },
+    { path: '/:notFound(.*)', component: () => import('../views/NotFoundView.vue') }
     // { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') }
   ]
 })
