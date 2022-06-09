82eaacf031608c9ba38edc512c7d911b73c854f5
Lucas <r.lucasmoretto@gmail.com>
2022-06-08 22:35:29 -0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/layout/Navbar.vue'
+import FooterComponent from './components/layout/FooterComponent.vue'
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <FooterComponent />
 </template>
 
 <style>
---
+++ src/components/layout/FooterComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <footer>
+        <div>
+            <p>Developed by </p>
+            <a href="https://github.com/LucasMorettoRodrigues/">
+                l.m.r
+                <i class="fa fa-github icon" style="font-size:36px"></i>
+            </a>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background-color: black;
+    width: 100%;
+}
+
+footer div {
+    padding: 30px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+footer div a {
+    color: white;
+    margin-left: 15px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+}
+
+footer div a:hover {
+    color: var(--primary-color-1);
+}
+
+.icon {
+    margin-left: 15px;
+}
+</style>
